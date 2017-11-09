b1de9a7c47a1eaf27602371c2d56acfc5168209c
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2017-11-09 14:33:53 +0800

    Update linearTest.js

--- linearTest.js
+++ linearTest.js
@@ -1,13 +1,4 @@
 /**
- * @fileoverview A VPAID ad useful for testing functionality of the sdk.
- * This particular ad will just play a video.
- *
- * @author ryanthompson@google.com (Ryan Thompson)
- */
-
-
-
-/**
  * @constructor
  */
 var VpaidVideoPlayer = function() {
@@ -42,66 +33,29 @@ var VpaidVideoPlayer = function() {
   this.attributes_ = {
     'companions' : '',
     'desiredBitrate' : 256,
-    'duration': 30,
+    'duration': 15,
     'expanded' : false,
     'height' : 0,
     'icons' : false,
     'linear' : true,
-    'remainingTime' : 13,
-    'skippableState' : false,
+    'remainingTime' : 15,
+    'skippableState' : true,
     'viewMode' : 'normal',
     'width' : 0,
     'volume' : 1.0
   };
 
-  /**
-   * @type {?number} id of the interval used to synch remaining time
-   * @private
-   */
-  this.intervalId_ = null;
-
-  /**
-   * A set of events to be reported.
-   * @type {Object}
-   * @private
-   */
-  this.quartileEvents_ = [
-    {event: 'AdImpression', value: 0},
-    {event: 'AdVideoStart', value: 0},
-    {event: 'AdVideoFirstQuartile', value: 25},
-    {event: 'AdVideoMidpoint', value: 50},
-    {event: 'AdVideoThirdQuartile', value: 75},
-    {event: 'AdVideoComplete', value: 100}
-  ];
-
-  /**
-   * @type {number} An index into what quartile was last reported.
-   * @private
-   */
-  this.lastQuartileIndex_ = 0;
-
-  /**
-   * An array of urls and mimetype pairs.
-   *
-   * @type {!object}
-   * @private
-   */
-  this.parameters_ = {};
+  
+  var data;
+  rubiconTimeout = true;
 };
 
 
 /**
  * VPAID defined init ad, initializes all attributes in the ad.  The ad will
  * not start until startAd is called.
- *
- * @param {number} width The ad width.
- * @param {number} height The ad heigth.
- * @param {string} viewMode The ad view mode.
- * @param {number} desiredBitrate The desired bitrate.
- * @param {Object} creativeData Data associated with the creative.
- * @param {Object} environmentVars Variables associated with the creative like
- *     the slot and video slot.
  */
+
 VpaidVideoPlayer.prototype.initAd = function(
     width,
     height,
@@ -115,18 +69,33 @@ VpaidVideoPlayer.prototype.initAd = function(
   this.attributes_['viewMode'] = viewMode;
   this.attributes_['desiredBitrate'] = desiredBitrate;
   this.slot_ = environmentVars.slot;
-  this.videoSlot_ = environmentVars.videoSlot;
 
   // Parse the incoming parameters.
-  this.parameters_ = JSON.parse(creativeData['AdParameters']);
+  data = JSON.parse(creativeData['AdParameters']);
+
+  this.imageUrls_ = data.overlays || [];
+  this.videos_ = data.videos || [];
+  this.accountId = data.accountId;
+  this.siteId = data.siteId;
+  this.zoneId = data.zoneId;
+  this.sizeId = data.sizeId;
+
+  zoneIdF = [this.zoneId, this.sizeId].join('-');
+  //this.sizeDimensionW = data.sizeDimension.split('x')[0];
+  //this.sizeDimensionH = data.sizeDimension.split('x')[1];
+  this.skippable = data.skippable;
+  this.skippableTime = data.skippableTime || 5;
+  this.timeoutRubicon = data.timeoutRubicon || 3;
+  this.timeoutDisplay = data.timeoutDisplay || 15;
+
+  this.attributes_['skippableState'] = this.skippable;
 
   this.log('initAd ' + width + 'x' + height +
       ' ' + viewMode + ' ' + desiredBitrate);
+ 
+
   this.callEvent_('AdLoaded');
 };
-
-
-
 
 
 /**
@@ -142,7 +111,6 @@ VpaidVideoPlayer.prototype.updateVideoPlayerSize_ = function() {
   } catch (e) { /* no op*/}
 };
 
-
 /**
  * Returns the versions of VPAID ad supported.
  * @param {string} version
@@ -153,50 +121,100 @@ VpaidVideoPlayer.prototype.handshakeVersion = function(version) {
 };
 
 
+VpaidVideoPlayer.prototype.createCloseButton = function() {
+
+ var skippableState = this.attributes_['skippableState'];
+  if (skippableState) {
+var closeButton = document.createElement("div");
+closeButton.id = "demo";
+closeButton.style.width="120px"
+closeButton.style.height="16px"
+closeButton.style.bottom="10px"
+closeButton.style.right="10px"
+closeButton.style.position="absolute"
+closeButton.style.display="inline-block"
+closeButton.style.background="white"
+window.document.body.insertAdjacentElement('afterbegin', closeButton);
+  this.countDownTimer2();
+}
+};
+
+
+VpaidVideoPlayer.prototype.countDownTimer2 = function() {
+// Set the date we're counting down to
+
+var CountDownTime = 5;
+// Update the count down every 1 second
+var CountDownTimer = setInterval(function() {
+if (CountDownTime < 0) {
+        clearInterval(CountDownTimer);
+        var closeButton = document.getElementById("demo");
+        closeButton.innerHTML = "Click To Skip Ad";
+        closeButton.style.cursor = "pointer";
+        closeButton.onclick = function(){
+          stopAd();
+        }       
+    }
+else{
+    var seconds = Math.floor(CountDownTime);
+  document.getElementById("demo").innerHTML = "Skip Ad in " + seconds + " s ";
+}
+   CountDownTime-- 
+}, 1000);
+};
+
 /**
  * Called by the wrapper to start the ad.
  */
 VpaidVideoPlayer.prototype.startAd = function() {
   this.log('Starting ad');
 
-function onAdsLoaded(response) {
-   if (response.status == "ok") {
-       var ad;
-       var html;
-       for (var i = 0; i < response.ads.length; i++) {
-           ad = response.ads[i];
-           if (ad.status == "ok") {
-               if (ad.type == "script") {
-                   document.write("<div id = 'test' style = 'width: 300px; height: 250px; top: 10%; margin: 0 auto; position: relative;'><script type='text/javascript'>"+ad.script+"</scr"+"ipt></div>"); 
-               }
-               if (ad.type == "html") {
-                   document.write(ad.html);
-               }
-           } else {
-                document.write("<div>status="+ad.status+"</div>");
-           }
-       }
-   }
-}
+ function onAdsLoaded(response) {
+   
+
+     if (response.status == "ok") {      
+         var ad;
+         var html;
+         for (var i = 0; i < response.ads.length; i++) {
+             ad = response.ads[i];
+             if (ad.status == "ok") {
+                 if (ad.type == "script") {
+                    document.write("<div id = 'test' style = 'width: 300px; height: 250px; top: 10%; margin: 0 auto; position: relative;'><script type='text/javascript'>"+ad.script+"</scr"+"ipt></div>"); 
+                 }
+                 if (ad.type == "html") {
+                    document.write("<div id = 'test' style = 'width: 300px; height: 250px; top: 10%; margin: 0 auto; position: relative;'>"+ad.html+"</div>");                
+                 }
+             } else {                
+             }
+         }
+     }
+     else {
+     }
+  }
+
+window.stopAd = this.stopAd.bind(this);
+window.adError = this.adError.bind(this);
+window.createCloseButton = this.createCloseButton.bind(this);
+
+   setTimeout(function(){
+    if (window.rubiconTimeout) {
+  stopAd(); }
+},this.timeoutRubicon);
 
   //add a test mute button
-var val1 = '<scr' + 'ipt type="text/javascript"> rp_account  = "8263"; rp_site      = "148426"; rp_zonesize  = "703002-15"; rp_adtype    = "jsonp"; rp_callback = '+onAdsLoaded+';rp_smartfile = "[SMART FILE URL]";</scr' + 'ipt>';
+var val1 = '<scr' + 'ipt type="text/javascript"> rp_account  = "'+this.accountId+'"; rp_site      = "'+this.siteId+'"; rp_zonesize  = "'+zoneIdF+'"; rp_adtype    = "jsonp"; rp_callback = '+onAdsLoaded+';rp_smartfile = "[SMART FILE URL]";</scr' + 'ipt>';
 document.write(val1);
 
 var val2 = '<scr' + 'ipt type="text/javascript" src="https://ads.rubiconproject.com/ad/8263.js"></scr' + 'ipt>';
-document.write(val2);    
+document.write(val2); 
 
-  //add a test mute button
-  var muteButton = document.createElement('input');
-  muteButton.setAttribute('type', 'button');
-  muteButton.setAttribute('value', 'mute/unMute');
+  setTimeout(function(){
+  stopAd();
+},this.timeoutDisplay);
 
-  muteButton.addEventListener('click',
-      this.muteButtonOnClick_.bind(this),
-      false);
-  this.slot_.appendChild(muteButton);
 
   this.callEvent_('AdStarted');
+  this.callEvent_('AdImpression');
 };
 
 
@@ -214,6 +232,10 @@ VpaidVideoPlayer.prototype.stopAd = function() {
   setTimeout(callback, 75, ['AdStopped']);
 };
 
+VpaidVideoPlayer.prototype.adError = function() {
+  this.log('adError');
+  this.callEvent_('AdError');
+};
 
 /**
  * @param {number} value The volume in percentage.
