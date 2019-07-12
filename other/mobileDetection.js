3997698de93ebd9a83775bbe621fc82d539bac9a
Antoine Jacquemin <ajacquem@gmail.com>
2019-07-12 11:07:50 +0800

    Update and rename prebidLazyLoad.js to mobileDetection.js

---
+++ other/mobileDetection.js
@@ -0,0 +1,26 @@
+(function detectmob() {
+    var gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+
+    if (navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        (function() {
+            gads.src = (useSSL ? 'https:' : 'http:') +
+'//ads.rubiconproject.com/prebid/11964_general_mobile_test.js';
+        })();
+    } else {
+        (function() {
+            gads.src = (useSSL ? 'https:' : 'http:') + '//ads.rubiconproject.com/prebid/11964_test.js';
+        })();
+    }
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+}());
