16c49b0499fc35ca6a43ed6869cd3b679e927696
Antoine Jacquemin <ajacquem@gmail.com>
2019-07-12 14:48:27 +0800

    Update updateCreative.py

--- googleApi/updateCreative.py
+++ googleApi/updateCreative.py
@@ -2,8 +2,6 @@ from server import services
 services.connectToNetwork(XXXX)
 print "connected"
 orderIds = [XXXX]
-# placementIds = [28608740]
-# adunits = [780773]
 
 def getAdunits():
     result = []
@@ -44,8 +42,7 @@ def update():
             if 'results' in response:
                 # print "Read "+str(len(response["results"]))
                 for line_item in response["results"]:
-                    #print "customFiel Id is" + test[id]
-                    #print "customFiel Name is" + test[id]
+
                     drop_down_custom_field_value = {
                         'customFieldId': XXXX,
                         'xsi_type': 'DropDownCustomFieldValue',
@@ -54,77 +51,18 @@ def update():
 
                     custom_field_values = [drop_down_custom_field_value]
                     line_item['customFieldValues'] = custom_field_values
+                    
+                    # to add or update the inventory or adUnit targeting                        
+                    line_item['allowOverbook'] = True
+                    line_item['skipInventoryCheck'] = True
+                    line_item['targeting']['inventoryTargeting']['targetedAdUnits'] = []
+                    line_item['targeting']['inventoryTargeting']['targetedPlacementIds'] = [29480009]
+                    
                     # line_item['costPerUnit']['currencyCode'] = 'USD'
+                    
                     # print line_item
                     updated_line_items.append(line_item)
                     continue
-                         
-                    # line_item['targeting']['inventoryTargeting']['targetedAdUnits'] = getAdunits()
-
-                    # line_item['targeting']['inventoryTargeting'] = {
-                    #     'targetedPlacementIds': placementIds
-                    # }
-
-                    # children = line_item['targeting']['customTargeting']['children'][0]['children']
-                    # for index, child in enumerate(children):  
-                    #     if (child['keyId'] == 159886):
-                    #         del children[index]
-
-                
-                    
-                    
-                    
-
-                    #customFieldValues[] = { customFieldId = 1263826 value = { dataType = "DROP_DOWN" value = "Option1" } xsi_type = "CustomFieldValue" }
-                    #line_item['customFieldValues'] == custom_field_values
-                     #['targetedPlacementIds'].append(4084125)
-
-#line_item.results[0]['customFieldValues'] = custom_field_values
-
-                    # print line_item['targeting']['customTargeting']
-                    # print line_item['targeting']['customTargeting']['children'][0]
-                    # key = line_item['targeting']['customTargeting']['children'][0]['children'][0]['keyId']
-                    # values = line_item['targeting']['customTargeting']['children'][0]['children'][0]['valueIds']
-
-                    # line_item['targeting']['customTargeting'] = {
-                    #     'xsi_type': 'CustomCriteriaSet',
-                    #     'logicalOperator': 'AND',
-                    #     'children': [{
-                    #         'xsi_type': 'CustomCriteria',
-                    #         'operator': 'IS',
-                    #         'keyId': key,
-                    #         'valueIds': values, 
-                    #     },{
-                    #         'xsi_type': 'CustomCriteria',
-                    #         'operator': 'IS_NOT',
-                    #         'keyId': 10737907,
-                    #         'valueIds': [447863213241], 
-                    #     }],
-                    # }
-            
-                    # line_item['targeting']['customTargeting']['children'].append({
-                    #     'xsi_type': 'CustomCriteriaSet',
-                    #     'logicalOperator': 'AND',
-                    #     'children': [{
-                    #         'xsi_type': 'CustomCriteria',
-                    #         'operator': 'IS_NOT',
-                    #         'keyId': 10737907,
-                    #         'valueIds': [447863213241], 
-                    #     }],
-                    # })
-
-                    # line_item['targeting']['customTargeting']['children'].append(('CustomCriteriaSet'){
-                    #     'logicalOperator': 'AND',
-                    #     'children': [('CustomCriteria'){
-                    #         'operator': 'IS_NOT',
-                    #         'keyId': 10737907,
-                    #         'valueIds': [447863213241], 
-                    #     }],
-                    # })
-                    
-                    
-                    # break
-
                     
                 if (len(updated_line_items) > 0):
                     line_items = batchCreate(services.LineItemService.updateLineItems, updated_line_items)
